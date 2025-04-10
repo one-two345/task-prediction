d4530cba4b7809db9edf26c7020c1d7c82b7ee10
one-two345 <129900924+one-two345@users.noreply.github.com>
2025-04-10 19:13:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-import streamlit as st
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, explained_variance_score
+import streamlit as st
 import pickle
 
 # Load model
